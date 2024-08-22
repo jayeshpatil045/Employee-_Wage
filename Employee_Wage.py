d51e338e9f52bbb03254ec7ff0e13d2bc3479e3b
jayeshpatil045 <jaypatil04012003@gmail.com>
2024-08-22 13:06:46 +0530

    [Jayesh Patil] add: This Program Calculate Daily Employee Wage

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -8,28 +8,47 @@
 
 '''
 import random
+WAGE_PER_HOUR = 20
+FULL_DAY_HOUR = 8
 def Check_Attendance():
     """
-    Description:
-        Randomly selects from a list to check if the employee is present or not.
-        If 0, the employee is absent; if 1, the employee is present.
+        Description:
+            Randomly selects from a list to check if the employee is present or not.
+            If 0, the employee is absent; if 1, the employee is present.
 
-    Parameter:
-        None
+        Parameter:
+            None
 
-    Return:
-        Returns 0 or 1 to indicate absence or presence, respectively.
+        Return:
+            Returns 0 or 1 to indicate absence or presence, respectively.
     """   
 
     attendance_list = [0, 1]
     random.shuffle(attendance_list) 
     return attendance_list[0]
 
+def Calculate_Daily_Employee_Wage():
+    """
+
+        Description:
+            Calculate Employee Daily wages, wage per hour is 20 and full day hour is 8. 
+
+        Parameter:
+            None
+
+        Return:
+            Multiplication of wage per hour and full day hours.
+
+
+    """
+    return WAGE_PER_HOUR * FULL_DAY_HOUR
+
+
 def main():
-    print("Welcome to Employee Wage Program ")
+    print("Welcome to Employee Wage Program")
     attendance = Check_Attendance()
     if attendance == 1:
-        print("Employee is Present ")
+         print(f"Your daily wages is {Calculate_Daily_Employee_Wage()}.")
     else:
         print("Employee is Absent ")
 
